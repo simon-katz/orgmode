12320b0e6d184bdceff205baf4c43cf7a609c135
Simon Katz <sk@nomistech.com>
2020-10-23 14:55:41 +0100

    Tweaks following fork

--- project.clj
+++ project.clj
@@ -1,9 +1,13 @@
-(defproject bnbeckwith/orgmode "0.7.5"
+(defproject com.nomistech/orgmode "0.8.0"
   :description "Org-mode parser"
-  :url "http://github.com/bnbeckwith/orgmode"
+  :url "https://github.com/simon-katz/orgmode"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [hiccup "1.0.4"]
-                 [org.apache.commons/commons-lang3 "3.1"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [hiccup "1.0.5"]
+                 [org.apache.commons/commons-lang3 "3.11"]]
+  :profiles {:dev {:dependencies [[midje "1.9.9"]]
+                   :plugins [[lein-midje "3.2.1"]]}}
+  :deploy-repositories
+  [["releases"  {:sign-releases false :url "https://clojars.org/repo"}]
+   ["snapshots" {:sign-releases false :url "https://clojars.org/repo"}]])
